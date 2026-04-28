eec10a9dffbdeb9f4353f569fbfeebc6fb4a3a5b
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-04-28 17:22:59 +0530

    Time: 100 ms (48.85%), Space: 117.9 MB (7.63%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,19 +1,37 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        List<Integer> arr = new ArrayList<>();
-        int base = grid[0][0];
+        ArrayList<Integer> arr = new ArrayList<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if ((grid[i][j] - base) % x != 0) return -1;
+                if((grid[i][j] - grid[0][0]) %x != 0) return -1;
                 arr.add(grid[i][j]);
             }
         }
         Collections.sort(arr);
-        int median = arr.get(arr.size()/2);
-        int operation = 0;
+        int median = (arr.get(arr.size()/2));
+        int count = 0;
         for(int i=0;i<arr.size();i++){
-            operation += Math.abs(arr.get(i) - median)/ x;
+            count+= Math.abs(arr.get(i) - median) / x;
         }
-        return operation;
+        return count;
     }
 }
+
+/* 2 5 
+   6 8
+
+   x = 2
+*/
+
+/* 2 4 
+   6 8
+
+   6
+
+x = 2
+2-> 4-> 6 -- 2 step
+4 -> 6 -- 1 step
+6 -> 6 -> 0 step
+8 -> 6 ->> 1 step
+
+*/
