f61058c57f2b815f2082194b95787312bd4b5e76
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-04-29 22:27:24 +0530

    Time: 1 ms (99.96%), Space: 42.5 MB (63.86%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,10 +3,16 @@ class Solution {
         long reversed = 0;
         while(x!=0){
             int rem = x%10;
-            reversed = (reversed*10) + rem;
+            reversed = reversed * 10 + rem;
             x = x/10;
         }
         if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) return 0;
         return (int)reversed;
     }
 }
+/* 123 
+3 
+3*10 + 2 = 32
+32 *10 + 1 = 321
+437474673437 
+*/
