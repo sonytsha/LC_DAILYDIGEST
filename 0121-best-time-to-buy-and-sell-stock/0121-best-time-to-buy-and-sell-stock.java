4539f044668c05e37e6cdb42730e6d656a5bf19f
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-04-30 21:26:56 +0530

    Time: 2 ms (39.2%), Space: 94.5 MB (32.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxi = 0;
         int buy = prices[0];
+        int maxProfit = 0;
         for(int i=1;i<prices.length;i++){
             int sell = prices[i];
-            if(sell > buy) {
-                int profit = sell - buy;
-                maxi = Math.max(profit, maxi);
-            }
-            else{
+            if(buy > sell){
                 buy = sell;
             }
+            else{
+                int profit = sell - buy;
+                maxProfit = Math.max(profit, maxProfit);
+            }
         }
-        return maxi;
+        return maxProfit;
     }
 }
