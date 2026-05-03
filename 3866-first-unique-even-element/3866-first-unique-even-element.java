669909318c259fa3aabe22de8d9fca6358aeb289
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-05-03 23:44:34 +0530

    Time: 3 ms (27.79%), Space: 47 MB (13.54%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.java
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -16,3 +16,34 @@ class Solution {
         return -1;
     }
 }
+
+/* 
+
+why this didnt work coz suppose 3 times it repeated, so first time it added in set , second time it removed and third time it added back in set, so it means even with duplicate its in set
+
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set = new LinkedHashSet<>();
+        for(int i=0;i<len;i++){
+            if(nums[i] % 2 == 0){
+                if(!set.isEmpty()){
+                    if(!set.contains(nums[i])){
+                        set.add(nums[i]);
+                    }
+                    else{
+                        set.remove(nums[i]);
+                    }
+                }
+                else{
+                    set.add(nums[i]);
+                }
+            }
+        }
+        if(set.isEmpty()) return -1;
+        Iterator<Integer> it = set.iterator();
+        return it.next();
+    }
+}
+
+*/
