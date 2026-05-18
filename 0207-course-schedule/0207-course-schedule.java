5db3c5a07fe71e62e0594c8ed60f0d1623e60b8a
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-05-18 21:21:04 +0530

    Time: 34 ms (5.36%), Space: 46.6 MB (97.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,60 @@
 class Solution {
-    boolean cycle = false;
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(cycle) return false;
+
         boolean[] visited = new boolean[numCourses];
+
         boolean[] temp = new boolean[numCourses];
-        for(int i=0;i<numCourses;i++){
+
+        for(int i = 0; i < numCourses; i++){
+
             if(!visited[i]){
-                dfs(i, prerequisites , visited, temp);
-                if(cycle) return false;
+
+                if(dfs(i, prerequisites, visited, temp)){
+                    return false;
+                }
             }
         }
-        for(int i=0;i<numCourses ;i++){
-            if(!visited[i]) return false;
-        }
+
         return true;
     }
-    public void dfs(int start , int[][] prerequisites, boolean[] visited, boolean[] temp){
-        if (cycle) return;
+
+    public boolean dfs(int start,
+                       int[][] prerequisites,
+                       boolean[] visited,
+                       boolean[] temp){
+
         visited[start] = true;
+
         temp[start] = true;
 
-        for(int i=0;i<prerequisites.length;i++){
+        for(int i = 0; i < prerequisites.length; i++){
+
             int curr1 = prerequisites[i][0];
+
             int curr2 = prerequisites[i][1];
+
             if(curr1 == start){
-                if(temp[curr2] == true) {
-                    cycle = true;
-                    return;
+
+                if(temp[curr2]){
+                    return true;
                 }
+
                 if(!visited[curr2]){
-                dfs(curr2, prerequisites, visited, temp);
+
+                    if(dfs(curr2,
+                           prerequisites,
+                           visited,
+                           temp)){
+
+                        return true;
+                    }
                 }
             }
         }
+
         temp[start] = false;
+
+        return false;
     }
 }
