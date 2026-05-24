d8327e6052513eeb0444dd903cd2acf8a912a05b
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-05-24 17:35:45 +0530

    Time: 0 ms (100%), Space: 43 MB (18.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,12 +3,13 @@ class Solution {
         int len = nums.length;
         if(len==1) return nums[0];
         if(len==2) return Math.max(nums[0],nums[1]);
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        int prev1 = nums[0];
+        int prev2 = Math.max(nums[0],nums[1]);
         for(int i=2;i<len;i++){
-            dp[i] = Math.max(dp[i-1] , nums[i]+dp[i-2]);
+            int current = Math.max(prev2 , prev1+nums[i]);
+            prev1 = prev2;
+            prev2 = current;
         }
-        return dp[len-1];
+        return prev2;
     }
 }
