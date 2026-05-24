cfca971b032597d52e4e7340d8e88f59b8de713f
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-05-24 18:08:44 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[j] = dp[j-1] + dp[j];
+            }
+        }
+        return dp[n-1];
+    }
+}
+
+
+/* 
+class Solution {
+    public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             dp[i][0] = 1;
@@ -15,6 +30,8 @@ class Solution {
         return dp[m-1][n-1];
     }
 }
+
+*/
 
 /* 
 
