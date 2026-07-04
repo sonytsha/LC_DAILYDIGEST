d97f95f24107621370dff98826cf822b96c75f6f
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-07-04 12:43:25 +0530

    Time: 21 ms (88.38%), Space: 165.5 MB (35.31%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,85 @@
+class Solution {
+
+    class Pair{
+        int city;
+        int weight;
+        Pair(int city, int weight){
+            this.city = city;
+            this.weight = weight;
+        }
+    }
+    public int minScore(int n, int[][] roads) {
+        ArrayList<Pair>[] graph = new ArrayList[n+1];
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=1;i<=n;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0;i<roads.length;i++){
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int w = roads[i][2];
+
+            graph[u].add(new Pair(v,w));
+            graph[v].add(new Pair(u,w));
+        }
+
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[] visited = new boolean[n+1];
+
+            queue.add(1);
+            visited[1] = true;
+
+
+            while(!queue.isEmpty()){
+                int node = queue.poll();
+                for(Pair next : graph[node]){
+                    ans = Math.min(ans, next.weight);
+                    if(visited[next.city] == false){
+                        visited[next.city] = true;
+                        queue.add(next.city);
+                    }
+                }
+            }
+        return ans;
+    }
+}
+
+
+
+
+/* class Solution {
+    public int minScore(int n, int[][] roads) {
+        int mini = Integer.MAX_VALUE;
+        for(int i=0;i<roads.length;i++){
+           // int startNode = roads[i][0];
+           // int endNode = roads[i][1];
+            int dist = roads[i][2];
+            mini = Math.min(mini, dist);
+        }
+        return mini;
+    }
+}
+
+Wrong Answer
+38 / 43 testcases passed
+
+Analysis
+
+Editorial
+Input
+n =
+36
+roads =
+[[7,11,418],[13,23,287],[16,25,7891],[15,7,9695],[4,3,9569],[17,7,1809],[14,3,4720],[14,4,6118],[9,2,4290],[32,17,5645],[14,16,426],[36,7,6721],[13,30,9444],[3,25,4635],[33,5,1669],[22,18,8910],[5,28,7865],[13,10,9466],[7,9,2457],[11,8,4711],[17,11,6308],[7,34,3789],[8,33,9659],[16,3,4187],[16,20,3595],[23,10,6251],[26,22,6180],[4,16,5577],[26,7,5398],[6,36,8671],[10,19,3028],[23,30,1330],[19,13,8315],[25,20,4740],[25,4,5818],[30,10,8030],[30,19,7527],[28,6,6804],[21,27,1746],[18,9,5189],[7,27,6560],[20,14,2450],[27,32,3951],[2,21,3927],[1,15,9283],[3,20,5428],[15,26,5871],[19,23,4533],[14,25,6992],[4,20,5831]]
+View less
+
+Use Testcase
+Output
+287
+Expected
+418
+
+*/
