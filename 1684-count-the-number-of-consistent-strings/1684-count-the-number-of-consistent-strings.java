9aeb266478c3d7d6580392f37ccb2112166e46c2
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-07-05 20:36:06 +0530

    Time: 20 ms (17.6%), Space: 48.1 MB (7.86%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -6,8 +6,8 @@ class Solution {
             int match = 0;
             int len1 = curr.length();
             for(int j=0;j<len1;j++){
-                char curr1 = curr.charAt(j);
-                if(!isPresent(allowed, curr1)){
+                String curr1 = curr.charAt(j) + "";
+                if(!allowed.contains(curr1)){
                     break;
                 }
                 else{
@@ -17,12 +17,5 @@ class Solution {
             if(match == len1) count++;
         }
         return count;
-    }
-    public boolean isPresent(String heh, char hm){
-        for(int i=0;i<heh.length();i++){
-            char ch = heh.charAt(i);
-            if(ch == hm) return true;
-        }
-        return false;
     }
 }
