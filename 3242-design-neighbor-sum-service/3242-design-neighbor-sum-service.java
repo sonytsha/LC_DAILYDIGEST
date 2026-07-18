e639e655a5c384ada80554a92d697184c6c39815
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-07-18 21:55:10 +0530

    Time: 22 ms (5.24%), Space: 47.3 MB (42.41%) - LeetHub

---
+++ 3242-design-neighbor-sum-service/3242-design-neighbor-sum-service.java
@@ -0,0 +1,68 @@
+class NeighborSum {
+    int[][] arr = new int[10][10];
+
+    public NeighborSum(int[][] grid) {
+        for(int i=0;i<10;i++){
+        Arrays.fill(arr[i],-1);
+        }
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                arr[i][j] = grid[i][j];
+            }
+        }
+    }
+    
+    public int adjacentSum(int value) {
+        int sum = 0;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                if(arr[i][j] == value){
+                    if(i > 0){
+                        if(arr[i-1][j] != -1) sum += arr[i-1][j];
+                    }
+                    if(i<arr.length - 1){
+                        if(arr[i+1][j] != -1) sum += arr[i+1][j];
+                    }
+                    if(j>0){
+                        if(arr[i][j-1] != -1) sum += arr[i][j-1];
+                    }
+                    if(j<arr.length - 1){
+                        if(arr[i][j+1] != -1) sum += arr[i][j+1];
+                    }
+                }
+            }
+        }
+        return sum; 
+    }
+    
+    public int diagonalSum(int value) {
+        int sum = 0;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                if(arr[i][j] == value){
+                    if(i>0 && j>0){
+                        if(arr[i-1][j-1] != -1) sum += arr[i-1][j-1];
+                    }
+                    if(i< arr.length - 1 && j<arr.length - 1){
+                        if(arr[i+1][j+1] != -1) sum += arr[i+1][j+1];
+                    }
+                    if(i> 0&& j<arr.length - 1){
+                        if(arr[i-1][j+1] != -1) sum += arr[i-1][j+1] ;
+                    }
+                    if(i<arr.length - 1 && j>0){
+                        if(arr[i+1][j-1] != -1) sum += arr[i+1][j-1];
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NeighborSum object will be instantiated and called as such:
+ * NeighborSum obj = new NeighborSum(grid);
+ * int param_1 = obj.adjacentSum(value);
+ * int param_2 = obj.diagonalSum(value);
+ */
