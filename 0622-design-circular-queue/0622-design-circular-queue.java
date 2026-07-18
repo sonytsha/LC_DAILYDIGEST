0378f8ff2fb3f4b827f020f519e8a09b83f8b13d
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-07-18 20:40:20 +0530

    Time: 4 ms (100%), Space: 46.5 MB (51.03%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.java
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -1,45 +1,40 @@
 class MyCircularQueue {
-    Deque<Integer> dequeue = new LinkedList<>();
+    ArrayList<Integer> arr = new ArrayList<>();
     int capacity;
     public MyCircularQueue(int k) {
         capacity = k;
     }
     
     public boolean enQueue(int value) {
-        if(isFull()) return false;
-        dequeue.addLast(value);
+        if(capacity == arr.size()) return false;
+        arr.add(value);
         return true;
     }
     
     public boolean deQueue() {
-        if (isEmpty()) {
-        return false;
-        }
-        dequeue.pollFirst();
+        if(arr.size() == 0) return false;
+        arr.remove(0);
         return true;
     }
     
     public int Front() {
-        if (isEmpty()) {
-            return -1;
-        }
-        return dequeue.peekFirst();
+        if(arr.size() ==0) return -1;
+        return arr.get(0);
     }
     
     public int Rear() {
-        if (isEmpty()) {
-            return -1;
-        }
-        return dequeue.peekLast();
+        if(arr.size() ==0) return -1;
+        return arr.get(arr.size()-1);
     }
     
     public boolean isEmpty() {
-        if(dequeue.size() == 0) return true;
+        if(arr.size() ==0) return true;
         return false;
     }
     
     public boolean isFull() {
-        return dequeue.size() == capacity;
+        if(arr.size() == capacity) return true;
+        return false;
     }
 }
 
