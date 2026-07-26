a9a5f3d48f6bb9a4e7caf869035f97527c736caf
Sony Shaw <142374226+sonytsha@users.noreply.github.com>
2026-07-26 12:11:01 +0530

    Time: 4 ms (82.41%), Space: 47.4 MB (82.01%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,6 +1,33 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        return Math.max((nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3]), (nums[nums.length-1] * nums[0] * nums[1]));
+        int top1 = Integer.MIN_VALUE, top2 = Integer.MIN_VALUE, top3 =Integer.MIN_VALUE;
+        int bottom1 = Integer.MAX_VALUE , bottom2 = Integer.MAX_VALUE, bottom3 = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > top1){
+                top3 = top2;
+                top2 = top1;
+                top1 = nums[i];
+            }
+            else if(nums[i] > top2){
+                top3 = top2;
+                top2 = nums[i];
+            }
+            else if(nums[i]> top3){
+                top3 = nums[i];
+            }
+            if(nums[i] < bottom1){
+                bottom3 = bottom2 ;
+                bottom2 = bottom1;
+                bottom1 = nums[i];
+            }
+            else if(nums[i] < bottom2){
+                bottom3 = bottom2;
+                bottom2 = nums[i];
+            }
+            else if(nums[i] < bottom3){
+                bottom3 = nums[i];
+            }
+        }
+        return Math.max(bottom1 * bottom2 * top1 , top1 * top2 * top3); 
     }
 }
